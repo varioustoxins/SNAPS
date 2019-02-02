16b0fac5eff39a961615b91653aa544de4d57ea8
aheyam <43552587+aheyam@users.noreply.github.com>
2019-02-02 14:32:53 +0000

    Lots of work on error_dist_test.py to make useful plots

--- python/error_dist_test.py
+++ python/error_dist_test.py
@@ -10,20 +10,22 @@ import numpy as np
 import pandas as pd
 from scipy.stats import norm, multivariate_normal, linregress
 from plotnine import *
-from plotnine.ggplot import save_as_pdf_pages
+#from plotnine.ggplot import save_as_pdf_pages
 from NAPS_importer import NAPS_importer
 from NAPS_assigner import NAPS_assigner
 from pathlib import Path
 from math import sqrt
-path = Path("/Users/aph516/GitHub/NAPS/")
-#path = Path("C:/Users/Alex/GitHub/NAPS/")
+#path = Path("/Users/aph516/GitHub/NAPS/")
+path = Path("C:/Users/Alex/GitHub/NAPS/")
 #path = Path("C:/Users/kheyam/Documents/GitHub/NAPS/")
-#%%
-#### Prepare to import all proteins in the testset
+#%% Prepare to import all proteins in the testset
 
-testset_df = pd.read_table(path/"data/testset/testset.txt", header=None, names=["ID","PDB","BMRB","Resolution","Length"])
-testset_df["obs_file"] = [path/"data/testset/simplified_BMRB"/file for file in testset_df["BMRB"].astype(str)+".txt"]
-testset_df["preds_file"] = [path/"data/testset/shiftx2_results"/file for file in testset_df["ID"]+"_"+testset_df["PDB"]+".cs"]
+testset_df = pd.read_table(path/"data/testset/testset.txt", header=None, 
+                           names=["ID","PDB","BMRB","Resolution","Length"])
+testset_df["obs_file"] = [path/"data/testset/simplified_BMRB"/file 
+                      for file in testset_df["BMRB"].astype(str)+".txt"]
+testset_df["preds_file"] = [path/"data/testset/shiftx2_results"/file 
+                      for file in testset_df["ID"]+"_"+testset_df["PDB"]+".cs"]
 testset_df.index = testset_df["ID"]
 
 importer = NAPS_importer()
@@ -44,7 +46,8 @@ for i in testset_df["ID"]:
     obs = pd.merge(obs, obs_m1, how="left", left_index=True, right_index=True)
     obs.index = obs["SS_name"]
     
-    preds = assigner.import_pred_shifts(testset_df.loc[i, "preds_file"], filetype="shiftx2")
+    preds = assigner.import_pred_shifts(testset_df.loc[i, "preds_file"], 
+                                        filetype="shiftx2")
     #Change Bs to Cs
     preds.loc[preds["Res_type"]=="B", "Res_type"] = "C"
     preds.loc[preds["Res_typem1"]=="B", "Res_typem1"] = "C"
@@ -56,7 +59,8 @@ for i in testset_df["ID"]:
     else:
         seq_df = pd.concat([seq_df, df], ignore_index=True)
 
-seq_df = seq_df[["ID","Res_N","Res_type_obs","Res_type_pred","Res_typem1_obs","Res_typem1_pred"]]
+seq_df = seq_df[["ID","Res_N","Res_type_obs","Res_type_pred",
+                 "Res_typem1_obs","Res_typem1_pred"]]
 seq_df = seq_df.fillna("NA")
 tmp1 = seq_df.loc[seq_df["Res_type_obs"]!=seq_df["Res_type_pred"],:]
 tmp1 = tmp1.loc[~tmp1["Res_type_obs"].isin(["NA"]) & ~tmp1["Res_type_pred"].isin(["NA"]),:]
@@ -66,14 +70,13 @@ tmp2 = tmp2.loc[~tmp2["Res_typem1_obs"].isin(["NA"]) & ~tmp2["Res_typem1_pred"].
 bad_res = pd.concat([tmp1, tmp2])
 bad_res = bad_res.drop_duplicates()
 
-#%%
+#%% Import the actual data
 
 importer = NAPS_importer()
 assigner = NAPS_assigner()
 
 atom_set = {"H","N","HA","C","CA","CB","Cm1","CAm1","CBm1"}
 
-#### Import the actual data
 obs_all = None
 for i in testset_df["ID"]:
 #for i in ["A063"]:
@@ -119,7 +122,8 @@ df = pd.merge(obs_all, preds_all, on=["ID","Res_N","Res_type", "Atom_type"],
 df = df.loc[~(df["ID"]+df["Res_N"].astype(str)).isin(bad_res["ID"]+bad_res["Res_N"].astype(str)),:]
 df = df.dropna(axis=0, subset=["Res_type","Res_typem1","SS_name", "Res_name"])
 df["ID_Res"] = df["ID"]+ " " + df["Res_name"].astype(str)
-df = df[["ID_Res", "ID","Res_name","Res_N","Res_type","Res_typem1","Atom_type","Shift_obs","Shift_pred"]]
+df = df[["ID_Res", "ID","Res_name","Res_N","Res_type","Res_typem1",
+         "Atom_type","Shift_obs","Shift_pred"]]
         
 # Calculate the prediction errors
 df["Delta"] = df["Shift_pred"] - df["Shift_obs"]
@@ -138,7 +142,8 @@ scale["CBm1"] = scale["CAm1"]
 scale = scale/scale["H"]
    
 # Make a wide data frame of the obs and preds values
-df_wide = df.drop(["Atom_type","Shift_obs", "Shift_pred", "Delta"], axis="columns").drop_duplicates()
+df_wide = df.drop(["Atom_type","Shift_obs", "Shift_pred", "Delta"], 
+                  axis="columns").drop_duplicates()
 df_wide.index = df_wide["ID_Res"]
 df_wide = df_wide.drop("ID_Res", axis="columns")
 obs_wide = pd.concat([df_wide,
@@ -173,7 +178,8 @@ def calc_dist_matrix(x1, x2, atoms, sf, na_value=np.nan):
                       reshape([len(x2.index),-1]).transpose())
         
         
-        delta2[atom] = pd.DataFrame(((x2_atom - x1_atom)/sf[atom])**2, index=x1.index, columns=x2.index)
+        delta2[atom] = pd.DataFrame(((x2_atom - x1_atom)/sf[atom])**2, 
+                                    index=x1.index, columns=x2.index)
         # Make a note of NA positions in delta, and set them to default value 
         na_mask = np.isnan(delta2[atom])
         delta2[atom][na_mask] = na_value
@@ -235,7 +241,7 @@ for id in df_wide["ID"].unique():
     else:
         rank_df = pd.concat([rank_df, tmp], ignore_index=True)
 
-#%%
+#%% Look at distribution of nearest neighbours and ranks
 
 # With this dataframe, we can now look at the distribution of ranks
 (~rank_df.isna()).sum()     # Summarise non-na values
@@ -293,105 +299,110 @@ plt = plt + xlab("Predicted shift") + ylab("Prediction error")
 plt.save(path/"plots/Error vs pred shift.pdf", height=210, width=297, units="mm")
 
 
-#%% Make a "normalised" chemical shift that accounts for residue type and observed shift
+#%% Make a "corrected" predicted shift that accounts for residue type and observed shift
+
 df2 = df.copy()
 
-df2["Res_norm"] = df2["Res_type"]
-df2.loc[df2["Atom_type"].isin(["Cm1","CAm1","CBm1"]),"Res_norm"] = df2.loc[df2["Atom_type"].isin(["Cm1","CAm1","CBm1"]),"Res_typem1"]
+df2["Res_cor"] = df2["Res_type"]
+df2.loc[df2["Atom_type"].isin(["Cm1","CAm1","CBm1"]),"Res_cor"] = (
+         df2.loc[df2["Atom_type"].isin(["Cm1","CAm1","CBm1"]),"Res_typem1"])
 
-df2["Delta_norm"] = np.nan
+df2["Delta_cor"] = np.nan
 lm_results = pd.DataFrame(columns=["Atom_type","Res_type","Grad","Offset"])
 
 for atom in atom_set:
-    for res in df2["Res_norm"].unique():
-        mask = (df2["Atom_type"]==atom) & (df2["Res_norm"]==res)
+    for res in df2["Res_cor"].unique():
+        mask = (df2["Atom_type"]==atom) & (df2["Res_cor"]==res)
         tmp = df2.loc[mask, ["Shift_obs", "Delta"]].dropna(how="any")
         try:
             lm = linregress(tmp["Shift_obs"], tmp["Delta"])
             lm_results.loc[atom+"_"+res, :] = [atom, res, lm[0], lm[1]]
-            df2.loc[mask,"Delta_norm"] = (df2.loc[mask,"Delta"] 
-                                          - lm[0]*df2.loc[mask,"Shift_obs"]
-                                          - lm[1])
+            df2.loc[mask,"Shift_pred_cor"] = (df2.loc[mask,"Shift_pred"] 
+                                              - lm[0]*df2.loc[mask,"Shift_obs"]
+                                              - lm[1])
+            df2.loc[mask,"Delta_cor"] = (df2.loc[mask,"Shift_pred_cor"] - 
+                                         df2.loc[mask,"Shift_obs"])
         except:
             print("Error: ",res, atom)
 
-plt = ggplot(data = df2.loc[df2["Res_norm"]=="N",:]) 
-plt = plt + geom_point(aes(x="Shift_obs", y="Delta", colour="Res_norm"))
-plt = plt + facet_wrap("Atom_type", scales="free")
-#plt = plt + xlab("Observed shift") + ylab("Prediction error")
-plt
+#### Make some graphs showing that the corrected predictions are better
 
-#%% Look at correlated errors bertween different atom types
+plt = ggplot(data = df2) 
+plt = plt + geom_density(aes(x="Delta_cor", 
+                             linetype=["Normalised"], 
+                             colour="Atom_type"))
+plt = plt + geom_density(aes(x="Delta", 
+                             linetype=["Original"], 
+                             colour="Atom_type")) 
+plt = plt + facet_wrap("Atom_type", scales="free_y")
+plt = plt + xlim(-5,5) + xlab("Prediction error")
+plt = plt + ggtitle("Normalised prediction error has a slightly tighter "+
+                    "distribution than the original predictions")
+plt = plt + scale_linetype_discrete(name="Prediction type")
+plt.save(path/"plots/Norm error distribution.pdf", 
+         height=210, width=297, units="mm")
+
+plt1 = ggplot(data = df2)
+plt1 = plt + geom_point(aes(x="Shift_obs", y="Delta_cor", colour="Res_cor"))
+plt1 = plt + facet_wrap("Atom_type", scales="free")
+plt1 = plt + xlab("Observed shift") + ylab("Normalised prediction error")
+plt1 = plt + ggtitle("Normalised prediction error is independent of observed shift...")
+
+plt2 = ggplot(data = df2)
+plt2 = plt + geom_point(aes(x="Shift_obs", y="Delta_cor", colour="Res_cor"))
+plt2 = plt + facet_wrap("Atom_type", scales="free")
+plt2 = plt + xlab("Observed shift") + ylab("Normalised prediction error")
+plt2 = plt + ggtitle("...but is dependent on predicted shift. But I don't think this is a problem.")
+
+save_as_pdf_pages(filename=path/"plots/Norm error vs shift.pdf",
+                  plots=[plt1, plt2])
+
+
+plt = ggplot(df2.loc[df2["Res_cor"]=="Y",:]) 
+plt = plt + geom_density(aes(x="Shift_obs", colour=["Obs"]))
+plt = plt + geom_density(aes(x="Shift_pred", colour=["Pred"]))
+plt = plt + geom_density(aes(x="Shift_pred-Delta+Delta_cor", colour=["Pred_corr"]))
+plt = plt + facet_wrap("Atom_type", scales="free")
+plt.save(path/"plots/tmp.pdf", height=210, width=297, units="mm")
+
+def dist_comparison_plots():
+    for res in df2["Res_cor"].unique():
+        plt = ggplot(df2.loc[df2["Res_cor"]==res,:]) 
+        plt = plt + geom_density(aes(x="Shift_obs", colour=["Obs"]))
+        plt = plt + geom_density(aes(x="Shift_pred", colour=["Pred"]))
+        plt = plt + geom_density(aes(x="Shift_pred-Delta+Delta_cor", colour=["Pred_corr"]))
+        plt = plt + facet_wrap("Atom_type", scales="free")
+        yield(plt)
+
+save_as_pdf_pages(filename=path/"plots/Obs pred shift distributions.pdf", 
+                  plots=dist_comparison_plots())
+
+#%% Look at correlated errors between different atom types
 delta_wide = df2.pivot(index="ID_Res", columns="Atom_type", values="Delta")
 delta_corr = delta_wide.corr()
+
+delta_cor_wide = df2.pivot(index="ID_Res", columns="Atom_type", values="Delta_cor")
+delta_cor_corr = delta_cor_wide.corr()
+
+# Save to files for use with NAPS_assigner
+delta_wide.mean().to_csv("../data/d_mean.csv")
+delta_wide.cov().to_csv("../data/d_cov.csv")
+delta_cor_wide.mean().to_csv("../data/dd_mean.csv")
+delta_cor_wide.cov().to_csv("../data/dd_cov.csv")
+
+obs_wide = df2.pivot(index="ID_Res", columns="Atom_type", values="Shift_obs")
+obs_corr = obs_wide.corr()
 
 # Something *really* weird is going on with the Delta values for A001 - they're 
 # clustering on only a few distinct values. Maybe shiftY is involved somehow? 
 
 ggplot(delta_wide) + geom_point(aes(x="CA", y="HA"))
 
+
+
 #%% Not updated below this point to account for long vs wide dataframe, so won't work.
 
-
-# Fit linear model between d_atom and atom_obs, and subtract to form dd_atom
-atoms_m1 = pd.Series(["Cm1","CAm1","CBm1"])
-lm_results = pd.DataFrame(columns=["Atom_type","Res_type","Grad","Offset"])
-for atom in atom_set:
-    df["dd_"+atom] = np.NaN
-    
-    for res in df["Res_type"].unique():
-        if atom in atoms_m1:
-            tmp = df.loc[df["Res_typem1"]==res, [atom+"_obs", "d_"+atom]].dropna(how="any")
-        else:
-            tmp = df.loc[df["Res_type"]==res, [atom+"_obs", "d_"+atom]].dropna(how="any")
-        try:
-            lm = linregress(tmp[atom+"_obs"], tmp["d_"+atom])
-            lm_results.loc[atom+"_"+res,:] = [atom, res, lm[0], lm[1]]
-            if atom in atoms_m1:
-                df.loc[df["Res_typem1"]==res, "dd_"+atom] = df.loc[df["Res_typem1"]==res, "d_"+atom] - lm[0]*df.loc[df["Res_typem1"]==res, atom+"_obs"] - lm[1]
-            else:
-                df.loc[df["Res_type"]==res, "dd_"+atom] = df.loc[df["Res_type"]==res, "d_"+atom] - lm[0]*df.loc[df["Res_type"]==res, atom+"_obs"] - lm[1]
-        except:
-            print("Error: ",res, atom)
-# For some reason this isn't working on the m1 atoms...
-
-# Fit linear model between d_atom and atom_obs
-#fit_results = pd.DataFrame(columns=["Res_type","Atom_type","Grad","Offset","r2","p-value","stderr"])
-#for res in df["Res_type"].unique():
-#    for atom in atoms_obs:
-#        tmp = df.loc[df["Res_type"]==res, [atom, "d_"+atom.split("_")[0]]].dropna(how="any")
-#        try:
-#            lm = linregress(tmp[atom], tmp["d_"+atom.split("_")[0]])
-#            fit_results.loc[res+"_"+atom,:] = [res, atom, lm[0], lm[1], lm[2]**2, lm[3], lm[4]]
-#        except:
-#            print("Error: ",res, atom)
-#fit_results = fit_results.sort_values(by="Atom_type")
-#fit_results["Grad"] = fit_results["Grad"].astype(float)
-#ggplot(data=fit_results) + geom_point(aes(y="Grad", colour="Res_type", x="Atom_type")) 
-
 #%% Calculate covariance matrixes with and without delta adjustment
-
-d_df = df.loc[:,["d_"+ atom for atom in a.pars["atom_set"]]]
-dd_df = df.loc[:,["dd_"+ atom for atom in a.pars["atom_set"]]]
-
-d_mean = d_df.mean()
-d_cov = d_df.cov()
-dd_mean = dd_df.mean()
-dd_cov = dd_df.cov()
-
-# Remove "d_" or "dd_" from index and column names
-d_mean.index = [s[2:] for s in d_mean.index]
-d_cov.index = [s[2:] for s in d_cov.index]
-d_cov.columns = [s[2:] for s in d_cov.columns]
-dd_mean.index = [s[3:] for s in dd_mean.index]
-dd_cov.index = [s[3:] for s in dd_cov.index]
-dd_cov.columns = [s[3:] for s in dd_cov.columns]
-
-# Save to files for use with NAPS_assigner
-d_mean.to_csv("../data/d_mean.csv")
-d_cov.to_csv("../data/d_cov.csv")
-dd_mean.to_csv("../data/dd_mean.csv")
-dd_cov.to_csv("../data/dd_cov.csv")
 
 abs_corr_matrix = abs(dd_df.corr())
 
@@ -433,33 +444,6 @@ mvn_d.pdf(obs1[9:18])
 
 from math import exp
 e = exp(mvn_full.logpdf(obs1) - mvn_obs.logpdf(obs1[0:9]))
-
-
-#%%
-#### Try out Yeo-Johnson transformation
-# This should transform a distribution to be more normal
-
-from YeoJohnson import YeoJohnson
-
-yj = YeoJohnson()
-test_data = df["d_CA"]
-
-
-
-# To work out the best value of lambda, we'll need to find the maximum likelihood value of lambda.
-results = pd.DataFrame(columns=["lam", "log_prob"], index=np.linspace(0,2,50))
-for l in np.linspace(0,2,50):
-    x = yj.fit(df["C_obs"].dropna(), l)
-    mu, sig = norm.fit(x)
-    p = sum(np.log(norm.pdf(x, mu, sig)))
-    results.loc[l,:] = [l, p]
-ggplot(data=results) + geom_line(aes(x="lam", y="log_prob", group=1))
-    
-trans_data = pd.DataFrame( {"test":df["C_obs"], "trans" : yj.fit(df["C_obs"], 1.1) })
-trans_data = pd.DataFrame( {"test":df["d_C"], "trans" : yj.fit(df["d_C"], 1.1) })
-
-ggplot(data=trans_data) + geom_density(aes(x="test")) + geom_density(aes(x="trans"), colour="red")
-
 
 
 #%%
