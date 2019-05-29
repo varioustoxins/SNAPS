92c480c944a8197314e11085e5e870fd400f0a68
Alex Heyam <aph516@biolmac338.york.ac.uk>
2019-05-29 15:29:44 +0100

    Improved advanced options on site

--- webApp/args.py
+++ webApp/args.py
@@ -10,6 +10,7 @@ class Args:
         self.directory = os.path.join(instance_path, 'tmp_' + str(uuid.uuid4()))
         self.shift_file = os.path.join(self.directory, 'shift.txt')
         self.pred_file = os.path.join(self.directory, 'pred.txt')
+        self.config_file = os.path.join(self.directory, 'config.txt')
         self.output_file = os.path.join(self.directory, 'results.txt')
         self.plot = {}
         self.plot_file = os.path.join(self.directory, 'plot.html')
@@ -17,16 +18,17 @@ class Args:
         self.pred_type = form['pred_type'].strip().lower()
 
     def argsToList(self):
-        return [
+        arg_list = [
             self.shift_file,
             self.pred_file,
             self.output_file,
             '--shift_type', self.shift_type,
             '--pred_type', self.pred_type,
             '--plot_file', self.plot_file,
-            '-c', '../config/config_plot.txt',
+            '-c', self.config_file,
             #'-l', '../output/test.log'
         ]
+        return arg_list
 
     def getResults(self):
         with open(self.output_file,mode='r') as f:
--- webApp/fileHandler.py
+++ webApp/fileHandler.py
@@ -16,7 +16,44 @@ def saveFiles(request, args):
         args.pred_type = 'shiftx2'
     else:
         request.files['predictedShiftsFile'].save(args.pred_file)
+    
+    #args.config_file = '../config/config_plot.txt'
+    makeConfigFile(request, args)
 
 def deleteFiles(args):
     if os.path.exists(args.directory) and os.path.isdir(args.directory):
         shutil.rmtree(args.directory)
+
+def makeConfigFile(request, args):
+    """Create a configuration file using user-submitted options"""
+    
+    f = open(args.config_file, "w+")
+    
+    # Write the constant parameters to the file
+    s = """
+# Configuration file for NAPS
+prob_method     pdf     # Method for calculating probability (options are cdf or pdf)
+pred_correction False   # Apply a linear correction to the predicted shifts
+pred_correction_file      ../config/lin_model_shiftx2.csv    # File containing parameters for linear correction to predicted shift
+delta_correlation_mean_file     ../config/d_mean.csv       # File containing mean prediction errors
+delta_correlation_cov_file      ../config/d_cov.csv        # File containing covariances between the prediction errors
+delta_correlation_mean_corrected_file     ../config/dd_mean.csv       # File containing mean prediction errors, assuming the predictions have been corrected
+delta_correlation_cov_corrected_file      ../config/dd_cov.csv        # File containing covariances between the prediction errors, assuming the predictions have been corrected
+alt_assignments 0       # Number of alternative assignments to generate
+#atom_sd "H:0.1711, N:1.1169, HA:0.1231, C:0.5330, CA:0.4412, CB:0.5163, C_m1:0.5530, CA_m1:0.4412, CB_m1:0.5163"    # Atom standard deviations. Comma separated.
+atom_sd "H:0.454, N:2.429, HA:0.227, C:1.030, CA:0.932, CB:1.025, C_m1:1.030, CA_m1:0.932, CB_m1:1.025"    # Atom standard deviations. Comma separated.
+plot_strips     True\n"""
+
+    # Write the user-submitted parameters
+    s += "pred_offset %s\n" % str(request.form.get("predResOffset"))
+    if request.form.get("deltaCorrelation")=="on":
+        s += "delta_correlation       True\n"
+    else:
+        s += "delta_correlation       False\n"
+    s += "use_ss_class_info       False\n"
+    s += """atom_set      "%s"\n""" % ",".join(request.form.getlist("atomType"))
+    #print(s)
+    f.write(s)
+
+    f.close()
+    
