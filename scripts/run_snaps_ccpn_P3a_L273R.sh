8953b5242d9f14975a8b2fc928e7be37507028ea
hannahmorris <hannahmorris2001@outlook.com>
2023-03-28 14:45:46 +0100

    files to run snaps with CCPN input for P3a_L273R

---
+++ scripts/run_snaps_ccpn_P3a_L273R.sh
@@ -0,0 +1,1 @@
+python/SNAPS.py data/P3a_L273R/ccpn_resonances.txt data/P3a_L273R/shiftx2.cs data/P3a_L273R/test_P3a_L273_ccpn.out --shift_type ccpn -c config/config_yaml.txt -l data/P3a_L273R/ccpn_log.txt
