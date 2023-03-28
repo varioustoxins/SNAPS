48831784a113a7f67217f6a30899faa5c759cbe6
hannahmorris <hannahmorris2001@outlook.com>
2023-03-28 15:34:12 +0100

    files to run snaps with Mars input and output for P3a_L273R

---
+++ scripts/run_snaps_mars_p3a_L2723R.sh
@@ -0,0 +1,2 @@
+python/SNAPS.py data/P3a_L273R/mars_shifts.txt data/P3a_L273R/shiftx2.cs data/P3a_L273R/test_P3a_L273_mars.out --shift_type mars -c config/config_yaml.txt -l data/P3a_L273R/mars_log.txt
+#--strip_plot_file strips_mars.html
