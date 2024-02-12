bbe51f896bed75452239d12acccb603bf9f88769
hannahmorris <hannahmorris2001@outlook.com>
2024-02-12 14:31:21 +0000

    Script for assigning a T AA to Q

---
+++ scripts/run_snaps_nef_P3a_L273R_241_250.sh
@@ -0,0 +1,3 @@
+cd ../python
+./SNAPS.py ../test_data/P3a_L273R_241_250.nef  ../test_data/P3a_L273R_241_250_shiftx2.cs ../test_data/test_P3a_L273_241_250_T2Q_nef.out --shift_type nef --aa_types ../test_data/test_residue_T2Q.txt -c ../config/config_yaml.txt -l ../data/P3a_L273R/nef_log_241_250_T2Q.txt
+# testing residues 241-250 in P3a when residue is being falsely assigned
